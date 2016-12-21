0491e7a984aa1fb11053177a45f0fad1c66271be
yfujita <yfujita@n2sm.net>
2016-12-21 19:09:50 +0900

    netty3 support

--- src/main/java/org/codelibs/elasticsearch/df/util/NettyUtils.java
+++ src/main/java/org/codelibs/elasticsearch/df/util/NettyUtils.java
@@ -4,117 +4,85 @@ import java.lang.reflect.Field;
 import java.security.AccessController;
 import java.security.PrivilegedAction;
 
-import org.apache.logging.log4j.Logger;
+import org.codelibs.elasticsearch.df.netty.Netty3HttpProvider;
+import org.codelibs.elasticsearch.df.netty.Netty4HttpProvider;
+import org.codelibs.elasticsearch.df.netty.NettyHttpProvider;
 import org.elasticsearch.ElasticsearchException;
-import org.elasticsearch.common.logging.Loggers;
-import org.elasticsearch.http.netty4.pipelining.HttpPipelinedRequest;
 import org.elasticsearch.rest.RestChannel;
-import io.netty.channel.Channel;
 
 public final class NettyUtils {
-    private static final String CHANNEL_FIELD_NAME = "channel";
-
     private static final String DELEGATE_FIELD_NAME = "delegate";
 
     private static final String PIPELINEDREQUEST_FIELD_NAME = "pipelinedRequest";
 
-    private static final Logger logger = Loggers.getLogger(NettyUtils.class);
+    private static boolean initialized = false;
 
-    private static Field channelField;
-
-    private static Field delegateField;
-
-    private static Field pipelinedRequestField;
+    private static boolean netty3 = false;
 
     private NettyUtils() {
     }
 
-    public static HttpPipelinedRequest pipelinedRequest(final RestChannel channel) {
-        try {
-            return (HttpPipelinedRequest)pipelinedRequestField.get(delegateField.get(channel));
-        } catch (Exception e) {
-            e.printStackTrace();
+    public static boolean isNetty3(final RestChannel channel) {
+        if (!initialized) {
+            updateFields(channel);
         }
-        return null;
+        return netty3;
     }
 
-    public static Channel getChannel(final RestChannel channel) {
+    public static boolean isNetty4(final RestChannel channel) {
+        return !isNetty3(channel);
+    }
+
+    public static NettyHttpProvider getHttpProvider(final RestChannel channel) {
         try {
-            if (channelField == null && delegateField == null && pipelinedRequestField == null) {
-                if (delegateField == null) {
-                    delegateField = AccessController
-                            .doPrivileged(new PrivilegedAction<Field>() {
-                                @Override
-                                public Field run() {
-                                    try {
-                                        final Field field = channel.getClass()
-                                                .getDeclaredField(
-                                                        DELEGATE_FIELD_NAME);
-                                        field.setAccessible(true);
-                                        return field;
-                                    } catch (final Exception e) {
-                                        throw new ElasticsearchException(e);
-                                    }
-                                }
-                            });
-                    channelField = AccessController
-                            .doPrivileged(new PrivilegedAction<Field>() {
-                                @Override
-                                public Field run() {
-                                    try {
-                                        final Field field = delegateField
-                                                .get(channel).getClass()
-                                                .getDeclaredField(
-                                                        CHANNEL_FIELD_NAME);
-                                        field.setAccessible(true);
-                                        return field;
-                                    } catch (final Exception e) {
-                                        throw new ElasticsearchException(e);
-                                    }
-                                }
-                            });
-                    pipelinedRequestField = AccessController
-                        .doPrivileged(new PrivilegedAction<Field>() {
-                            @Override
-                            public Field run() {
-                                try {
-                                    final Field field = delegateField
-                                        .get(channel).getClass()
-                                        .getDeclaredField(
-                                            PIPELINEDREQUEST_FIELD_NAME);
-                                    field.setAccessible(true);
-                                    return field;
-                                } catch (final Exception e) {
-                                    throw new ElasticsearchException(e);
-                                }
-                            }
-                        });
-                } else if (channelField == null) {
-                    channelField = AccessController
-                            .doPrivileged(new PrivilegedAction<Field>() {
-                                @Override
-                                public Field run() {
-                                    try {
-                                        final Field field = channel.getClass()
-                                                .getDeclaredField(
-                                                        CHANNEL_FIELD_NAME);
-                                        field.setAccessible(true);
-                                        return field;
-                                    } catch (final Exception e) {
-                                        throw new ElasticsearchException(e);
-                                    }
-                                }
-                            });
-                }
+            if (isNetty3(channel)) {
+                return new Netty3HttpProvider(channel);
+            } else {
+                return new Netty4HttpProvider(channel);
             }
-            if (delegateField != null) {
-                return (Channel) channelField.get(delegateField.get(channel));
-            } else if (channelField != null) {
-                return (Channel) channelField.get(channel);
-            }
-        } catch (final Exception e) {
-            logger.error("Could not load Netty's channel.", e);
+        } catch (IllegalAccessException e) {
+            throw new ElasticsearchException("Could not get http provider.");
         }
-        return null;
+    }
+
+    public synchronized static void updateFields(final RestChannel channel) {
+        final Field delegateField = AccessController
+            .doPrivileged(new PrivilegedAction<Field>() {
+                @Override
+                public Field run() {
+                    try {
+                        final Field field = channel.getClass()
+                            .getDeclaredField(
+                                DELEGATE_FIELD_NAME);
+                        field.setAccessible(true);
+                        return field;
+                    } catch (final Exception e) {
+                        throw new ElasticsearchException(e);
+                    }
+                }
+            });
+
+        try {
+            AccessController
+                .doPrivileged(new PrivilegedAction<Field>() {
+                    @Override
+                    public Field run() {
+                        try {
+                            final Field field = delegateField
+                                .get(channel).getClass()
+                                .getDeclaredField(
+                                    PIPELINEDREQUEST_FIELD_NAME);
+                            field.setAccessible(true);
+                            return field;
+                        } catch (final IllegalAccessException | NoSuchFieldException e) {
+                            throw new ElasticsearchException(e);
+                        }
+                    }
+                });
+            netty3 = false;
+        } catch (ElasticsearchException e) {
+            netty3 = true;
+        }
+        initialized = true;
     }
 }
